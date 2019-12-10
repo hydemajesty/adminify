163e0f05135105a433aa3110f224f8f0a8d167a1
yi.cheng <yi.cheng@capitaledge.cn>
2019-12-10 10:28:15 +0800

    升级vuetify版本，调整首页结构

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-app :dark="dark" standalone>
-    <v-navigation-drawer v-model='drawer' clipped :mobile-break-point="0" enable-resize-watcher :dark="dark">
+  <v-app :dark="dark">
+    <v-navigation-drawer app clipped :mobile-break-point="0"  :dark="dark" v-model='drawer'>
       <div style="padding-left:5em">
         <v-switch :label="(!dark ? 'Light' : 'Dark') + ' Theme'" v-model="dark" :dark="dark" hide-details></v-switch>
       </div>
@@ -8,7 +8,7 @@
       </v-divider>
       <v-list dense>
         <template v-for='item in menu'>
-          <v-list-group v-if='item.items' v-bind:group='item.group'>
+          <v-list-group v-if='item.items' :group='item.group' :key='item'>
             <v-list-tile :to='item.href' slot='item' :title="item.title">
               <v-list-tile-action>
                 <v-icon> {{item.icon}}</v-icon>
@@ -20,7 +20,7 @@
                 <v-icon>keyboard_arrow_down</v-icon>
               </v-list-tile-action>
             </v-list-tile>
-            <v-list-tile v-for='subItem in item.items' :key='subItem.href' :to='subItem.href' v-bind:router='!subItem.target' ripple v-bind:disabled='subItem.disabled' v-bind:target='subItem.target'>
+            <v-list-tile v-for='subItem in item.items' :key='subItem.href' :to='subItem.href' :router='!subItem.target' ripple :disabled='subItem.disabled' :target='subItem.target'>
               <v-list-tile-action v-if='subItem.icon'>
                 <v-icon>{{ subItem.icon }}</v-icon>
               </v-list-tile-action>
@@ -29,9 +29,9 @@
               </v-list-tile-content>
               </v-list-tile>
             </v-list-group>
-            <v-subheader v-else-if='item.header'>{{ item.header }}</v-subheader>
-            <v-divider v-else-if='item.divider'></v-divider>
-            <v-list-tile v-else :to='item.href' router ripple v-bind:disabled='item.disabled' :title="item.title">
+            <v-subheader v-else-if='item.header' :key='item.header'>{{ item.header }}</v-subheader>
+            <v-divider v-else-if='item.divider' :key='item.divider'></v-divider>
+            <v-list-tile v-else :to='item.href' router ripple :disabled='item.disabled' :title="item.title" :key='item'>
               <v-list-tile-action>
                 <v-icon>{{ item.icon }}</v-icon>
               </v-list-tile-action>
@@ -45,9 +45,9 @@
         </template>
       </v-list>
     </v-navigation-drawer>
-    <!-- <v-toolbar app clipped-left>
+    <v-toolbar app clipped-left :class="theme">
       <v-layout align-center>
-        <v-toolbar-side-icon  @click="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-side-icon  dark @click="drawer = !drawer"></v-toolbar-side-icon>
         <v-toolbar-title slot="activator">
           <v-avatar tile v-if="logo">
             <v-img :src="logo" :alt="company"></v-img>
@@ -56,40 +56,25 @@
         </v-toolbar-title>
         <v-spacer></v-spacer>
         <v-menu offset-y>
-          <v-btn icon, dark, slot="activator">
+          <v-btn icon dark slot="activator">
             <v-icon dark> format_paint </v-icon>
           </v-btn>
           <v-list v-for="n in colors" :key="n" :class="n" @mouseover.native="theme = n"></v-list>
         </v-menu>
       </v-layout>
     </v-toolbar>
-    <v-content>
-      <v-container fluid>
-        <v-fade-transition mode="out-in">
-          <router-view></router-view>
-        </v-fade-transition>
-      </v-container>
-    </v-content>
-    <v-footer app fixed height="auto" class="elevation-2">
-      <v-card class="flex" flat tile>
-        <v-card-actions class="justify-center">{{ copyright }}</v-card-actions>
-      </v-card>
-    </v-footer> -->
-
-
-
-
-
     <main>
       <v-container fluid>
-        <v-alert v-bind='message' v-model='message.body' dismissible>{{message.body}}
           <v-slide-y-transition mode='out-in'>
             <router-view></router-view>
           </v-slide-y-transition>
-        </v-alert>
       </v-container>
     </main>
-
+    <v-footer app fixed height="auto" class="elevation-2" text-xs-center>
+      <v-card class="flex" flat tile>
+        <v-card-actions class="justify-center">{{ copyright }}</v-card-actions>
+      </v-card>
+    </v-footer>
   </v-app>
 
 </template>
